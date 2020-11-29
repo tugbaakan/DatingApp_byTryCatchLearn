dd7e9f079a50516a37fd927cba5a96143a590e54
tugbaakan <akan.tugba@gmail.com>
2020-11-29 18:46:43 +0300

    Initial commit

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,23 @@
+using System;
+using API.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace API.Data
+{
+    public class DataContext :DbContext
+    {
+        public DataContext(DbContextOptions options): base(options)
+        {
+
+
+
+        }
+
+        public DbSet<AppUser> Users{ get; set; }
+        
+    }   
+}
